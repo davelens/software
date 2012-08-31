e913c7477a155d4d758021969b3b52de0c8e5101
Dave Lens <dave.lens@wijs.be>
2012-08-31 14:41:18 +0200

    query the user to install additional applications

--- install.sh
+++ install.sh
@@ -16,4 +16,7 @@ brew install https://raw.github.com/Homebrew/homebrew-dupes/master/vim.rb
 # Use brew to install mysql, php+apc, pear, and configure Apache/PHP
 . install_mamp_stack.sh
 
+# Query the user if he/she wants to download/install additional apps
+. install_apps.sh
+
 echo "$(tput setaf 10)Done.$(tput sgr0)"
