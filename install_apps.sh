14e55771634a9ea16e06afc1bdce6fcb3d30c485
davelens <github@davelens.be>
2013-12-02 00:20:50 +0100

    remove a few apps from install_apps

--- install_apps.sh
+++ install_apps.sh
@@ -31,20 +31,6 @@ if [ $(lowercase $downloadDMGFiles) == "y" ]; then
 	else
 		echo "$(tput setaf 10)Alfred is already installed.$(tput sgr0)"
 	fi
-
-	# Latest Firefox
-	if [ ! -d ~/Applications/Firefox.app ]; then
-		download_dmg "$(curl -s http://www.mozilla.org/en-US/firefox/fx/?from=getfirefox#desktop | grep 'Mac OS X')" ~/Downloads/firefox.dmg
-	else
-		echo "$(tput setaf 10)Firefox is already installed.$(tput sgr0)"
-	fi
-
-	# MySQL Workbench
-	if [ ! -d ~/Applications/MySQLWorkbench.app ]; then
-		wget -O ~/Downloads/mysql-workbench.dmg http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.41-osx-i686.dmg
-	else
-		echo "$(tput setaf 10)MySQLWorkbench is already installed.$(tput sgr0)"
-	fi
 else
 	echo ""
 fi
