7f33e58f105baad3187a89aa35a7af8b9c38a7cf
davelens <github@davelens.be>
2014-07-15 21:18:24 +0200

    fix formatting

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,22 +5,22 @@
 
 # Check if an id_rsa key is set; we'll need it for github auth
 if [[ ! -f ~/.ssh/id_rsa ]]; then
-	echo "$(tput setaf 9)"
-	echo "ERROR: You do not have an SSH key set yet. You need one linked to your GitHub account."
-	echo "Ignore this error if have one that is not named id_rsa."
-	exit 1
+  echo "$(tput setaf 9)"
+  echo "ERROR: You do not have an SSH key set yet. You need one linked to your GitHub account."
+  echo "Ignore this error if have one that is not named id_rsa."
+  exit 1
 fi
 
 # Install homebrew
 if [[ $(which brew) == "" ]]; then
-	echo "$(tput setaf 11)"
-	echo "Installing Homebrew..."
-	echo "$(tput sgr0)"
+  echo "$(tput setaf 11)"
+  echo "Installing Homebrew..."
+  echo "$(tput sgr0)"
 
-	/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
-	brew doctor
-	brew update
-	brew upgrade
+  /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+  brew doctor
+  brew update
+  brew upgrade
 
   # Install CLI tools
   if [[ $(which gcc) != "" ]]; then
@@ -32,11 +32,11 @@ if [[ $(which brew) == "" ]]; then
   brew tap caskroom/cask
   brew install brew-cask
 
-	# Install the git and compilation dependencies & tools
-	brew tap homebrew/dupes
-	brew install autoconf automake
-	brew install libtool libyaml libxml2 libxslt libksba openssl sqlite
-	brew install mercurial
-	brew install git
-	brew install cmake
+  # Install the git and compilation dependencies & tools
+  brew tap homebrew/dupes
+  brew install autoconf automake
+  brew install libtool libyaml libxml2 libxslt libksba openssl sqlite
+  brew install mercurial
+  brew install git
+  brew install cmake
 fi
