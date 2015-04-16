c89bab78ffbb2953bb7d0772af709d576c8d696a
davelens <github@davelens.be>
2015-04-16 18:49:39 +0200

    rename variable

--- install_apps.sh
+++ install_apps.sh
@@ -6,9 +6,9 @@
 
 echo ""
 echo -n "$(tput setaf 11)Would you like to download additional software?$(tput sgr0) $(tput bold)(y/n)$(tput setaf 11):$(tput sgr0) "
-read downloadDMGFiles
+read install_apps
 
-if [ $(lowercase $downloadDMGFiles) == "y" ]; then
+if [ $(lowercase $install_apps) == "y" ]; then
   echo 'Now would be a good time to grab a coffee, this will probably take a while.'
   brew cask install alfred
   brew cask install firefox
